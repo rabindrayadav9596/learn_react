a356930ecb1ffe90b47e9fa9bd05131484e4e454
Rabindra Yadav <rabindrayadav9596@gmail.com>
2020-06-29 01:54:32 -0500

    upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+// import Greet from "./components/Greet";
+// import Welcome from "./components/Welcome";
+import Message from "./components/Message";
+function App() {
+  return (
+    <div className="App">
+      <Message />
+    </div>
+  );
+}
+
+export default App;
